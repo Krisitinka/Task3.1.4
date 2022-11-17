e4acf3719396f9fc852de4961fed46d0f57a451d
belya <belyakova.kristinochcka@yandex.ru>
2022-11-17 23:30:18 +0500

    Second.

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UsersController.java
@@ -0,0 +1,21 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+@Controller
+@RequestMapping("/user")
+public class UsersController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping()
+    public String showOneUsers() {
+        return "userPanel";
+    }
+
+}
